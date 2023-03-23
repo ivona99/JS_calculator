ec948e75f21507a7b56de28d5f5e0262d04c6e6d
ivona99 <57632353+ivona99@users.noreply.github.com>
2023-03-23 20:52:48 +0100

    decimal key

--- script.js
+++ script.js
@@ -27,35 +27,52 @@ calculator.addEventListener('click', e => {
     const previousKeyType = container.dataset.previousKeyType;
     
     Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
-
+    
     if(!action) {
+        
         if(displayedNum === '0'){
             display.textContent = keyContent;
         }else if(previousKeyType === 'operator'){
             display.textContent += keyContent;
-            container.dataset.previousKeyType = 'number';
+            // container.dataset.previousKeyType = 'number';
         }else {
             display.textContent = displayedNum + keyContent;
         }
+        container.dataset.previousKeyType = 'number';
+       
     }
 
     if(action === 'add' ||
     action === 'subtract' ||
     action === 'multiply' ||
     action === 'divide') {
+        
         key.classList.add('is-depressed');
-        display.textContent += keyContent;
         container.dataset.previousKeyType = 'operator';
+        
+        console.log("previous in operation", previousKeyType);
+        display.textContent += keyContent;
+        
         container.dataset.firstValue = displayedNum;
         container.dataset.operator = action;
+        
+        
     }
     if(action === 'decimal') {
-        display.textContent = displayedNum + '.';
+        if (!displayedNum.includes('.')) {
+            display.textContent = displayedNum + '.'
+          }if (previousKeyType === 'operator') {
+            display.textContent = '0.'
+          }
+          container.dataset.previousKeyType = 'decimal';
     }
+  
     if(action === 'clear') {
+        container.dataset.previousKeyType = 'clear'
         console.log("clear key");
     }
     if(action === 'calculate') {
+        container.dataset.previousKeyType = 'calculate'
         const firstValue = container.dataset.firstValue;
         const operator = container.dataset.operator;
         const secondValue = displayedNum;
