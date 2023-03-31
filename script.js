39cdb048900266c6b5ef6ef1d697ae6b8104ee51
ivona99 <57632353+ivona99@users.noreply.github.com>
2023-03-31 19:47:02 +0200

    bacis opeartions

--- script.js
+++ script.js
@@ -73,13 +73,6 @@ class Calculator {
 
 }
 
-
-
-// const container = document.querySelector('.container');
-// const calculator = document.querySelectorAll('.item');
-// const display = document.querySelector('.total');
-// const equal = document.querySelector('.back-green');
-
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
@@ -119,118 +112,6 @@ deleteButton.addEventListener('click', button => {
     calculator.updateDisplay();
 })
 
-// calculator.forEach(function(btn){
-//     btn.addEventListener('click', (e) => {
-//         const key = e.target;
-//         const keyContent = key.textContent;
-//         const displayedNum = display.textContent;
-        
-//         if(displayedNum === '0') {
-//             display.textContent = keyContent;
-//         }else {
-//             display.textContent = displayedNum + keyContent;
-//         }
-//     })
-// });
-
-
-
-
-
-
-// const calculate = (n1, operator, n2) => {
-//     let result = ''
-    
-//     if (operator === 'add') {
-//       result = parseFloat(n1) + parseFloat(n2)
-//     } else if (operator === 'subtract') {
-//       result = parseFloat(n1) - parseFloat(n2)
-//     }else if (operator === 'multiply') {
-//       result = parseFloat(n1) * parseFloat(n2)
-//     } else if (operator === 'divide') {
-//       result = parseFloat(n1) / parseFloat(n2)
-//     }
-    
-//     return result
-//   }
-
-// calculator.addEventListener('click', e => {
-//     if(e.target.matches('button'));
-//     const key = e.target;
-//     const action = key.dataset.action;
-//     const keyContent = key.textContent;
-//     const displayedNum = display.textContent;
-//     const previousKeyType = container.dataset.previousKeyType;
-
-//     Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
-    
-//     if(!action) {
-        
-//         if(displayedNum === '0'){
-//             display.textContent = keyContent;
-//         }else if(previousKeyType === 'operator'){
-//             display.textContent += keyContent;
-//             container.dataset.previousKeyType = 'number';
-//         }else {
-//             display.textContent = displayedNum + keyContent;
-//         }
-//         container.dataset.previousKeyType = 'number';
-       
-//     }
-
-//     if(action === 'add' ||
-//     action === 'subtract' ||
-//     action === 'multiply' ||
-//     action === 'divide') {
-
-//         const firstValue = container.dataset.firstValue;
-//         const operator = container.dataset.operator;
-//         const secondValue = displayedNum;;
-//         if (
-//             firstValue &&
-//             operator &&
-//             previousKeyType !== 'operator'
-//           ) {
-//             const calcValue = calculate(firstValue, operator, secondValue)
-//             display.textContent = calcValue
-            
-//           // Update calculated value as firstValue
-//             container.dataset.firstValue = calcValue;
-//           } else {
-//             // If there are no calculations, set displayedNum as the firstValue
-//             container.dataset.firstValue = displayedNum;
-//           }
-
-//         key.classList.add('is-depressed');
-//         container.dataset.previousKeyType = 'operator';
-//         display.textContent += keyContent;
-//         container.dataset.firstValue = displayedNum;
-//         container.dataset.operator = action;
-//     }
-//     if(action === 'decimal') {
-//         if (!displayedNum.includes('.')) {
-//             display.textContent = displayedNum + '.'
-//           }if (previousKeyType === 'operator') {
-//             display.textContent = '0.'
-//           }
-//           container.dataset.previousKeyType = 'decimal';
-//     }
-  
-//     if(action === 'clear') {
-//         container.dataset.previousKeyType = 'clear'
-//         console.log("clear key");
-//     }
-//     if(action === 'calculate') {
-//         const firstValue = container.dataset.firstValue;
-//         const operator = container.dataset.operator;
-//         const secondValue = displayedNum.charAt(2);
-
-//         if (firstValue) {
-//             display.textContent = calculate(firstValue, operator, secondValue);
-//           }
-//         container.dataset.previousKeyType = 'calculate';
-//     }
-// });
 
 
 
